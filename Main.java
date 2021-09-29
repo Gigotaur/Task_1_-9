9ff5bdc03e356503f8f83726837adf752d51f376
Gigotaur <90615070+Gigotaur@users.noreply.github.com>
2021-09-29 16:00:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner length = new Scanner(System.in);
-    double A_side, B_side;
-    System.out.println("Введите значение первого катета прямоугольного треугольника");
-    A_side = length.nextDouble();
+        double side_A, side_B;
+        double area = counterArea();
+        double perimetr = counterPerimetr();
+        Scanner length = new Scanner(System.in);
+        System.out.println("Введите значение первого катета прямоугольного треугольника");
+        side_A = length.nextDouble();
         Scanner width = new Scanner(System.in);
         System.out.println("Введите значение второго катета прямоугольного треугольника");
-        B_side = width.nextDouble();
-        double C_side = Math.sqrt(A_side*A_side+B_side*B_side);
-       // System.out.println(C_side);
-        double Square =(A_side*B_side/2);
-        System.out.println("Площадь прямоугольного треугольника равна: " + Square);
-        double Perimetor =(A_side+B_side+C_side);
-        System.out.println("Периметор прямоугольногот реугольника равен: " + Perimetor);
+        side_B = width.nextDouble();
+        return (side_A, side_B);
+    }
+    private static double Scanner () {
+        
+        return;
+    }
+
+
+    private static double counterArea () {
+        double side_C = Math.sqrt(side_A * side_B + side_B * side_B);
+        // System.out.println(side_C);
+        area = (side_A * side_B / 2);
+        System.out.println("Площадь прямоугольного треугольника равна: " + area);
+        return area;
+    }
+
+    private static double counterPerimetr () {
+        perimetr = (side_A + side_B + side_C);
+        System.out.println("Периметор прямоугольногот реугольника равен: " + perimetr);
+        return perimetr;
+    }
+    private static double checkThisOut () {
+        
     }
 }
